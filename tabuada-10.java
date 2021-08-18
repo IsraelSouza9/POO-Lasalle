cd4e87e769a1793d3863a29dcacaab8d19f2a881
Israel Souza <51099010+IsraelSouza9@users.noreply.github.com>
2021-08-18 08:50:21 -0300

    Add files via upload

---
+++ tabuada-10.java
@@ -0,0 +1,13 @@
+/*
+Israel Alessandro Souza da Costa
+0050015070
+*/
+class Main {
+  public static void main(String[] args) {
+    for (int i = 10; i<=10; i++){
+			for (int j = 1; j<=10; j++){
+				System.out.println (i+" x "+ j + " = " + i*j);
+			}
+		}
+  }
+}
