5a8686ce7b43681dfcfbf7844a49cc50cbd1e664
Israel Souza <51099010+IsraelSouza9@users.noreply.github.com>
2021-08-18 08:51:39 -0300

    Update tabuada-10.java

--- tabuada-10.java
+++ tabuada-10.java
@@ -5,9 +5,9 @@ Israel Alessandro Souza da Costa
 class Main {
   public static void main(String[] args) {
     for (int i = 10; i<=10; i++){
-			for (int j = 1; j<=10; j++){
-				System.out.println (i+" x "+ j + " = " + i*j);
-			}
-		}
+	for (int j = 1; j<=10; j++){
+	 System.out.println (i+" x "+ j + " = " + i*j);
+	}
+     }
   }
 }
